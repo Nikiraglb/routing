da8dd93cbc5e41a39bc0e6b8e24be68958b820f9
Nikita Golybev <nikitagolybev250505@gmail.com>
2023-05-19 16:53:00 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./style.css"
+
+function App() {
+  return (
+    <div className="App">
+      <a href="#">Статьи</a>
+      <a href="#">Тесты</a>
+      <a href="#">Игра</a>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Stat.js
@@ -0,0 +1,10 @@
+const Stat = () => {
+    return (
+        <>
+        <a href="#">111</a>
+        <a href="#">222</a>
+        </>
+    );
+}
+
+export default Stat;
